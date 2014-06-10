14686b960605fbf842876bbfe82fe580d43cc92f
MaskaZoRRo <kate_zen@inbox.ru>
2014-06-10 16:46:07 +0300

    ProductView.java add Javadoc

--- MozartTest/src/com/redlabrat/mozarttest/View/ProductView.java
+++ MozartTest/src/com/redlabrat/mozarttest/View/ProductView.java
@@ -2,9 +2,16 @@ package com.redlabrat.mozarttest.View;
 
 import android.content.Context;
 import android.widget.FrameLayout;
-
+/**
+ * Class represents how the Product will be shown at the Screen
+ * @author Alexandr Brich
+ * @version 1.1
+ */
 public class ProductView extends FrameLayout {
-
+	/**
+	 * Method send the context of the application to the super class constructor 
+	 * @param context Context of application
+	 */
 	public ProductView(Context context) {
 		super(context);
 	}
